f083b2aa18df4dd683647be0c8c5a89414689c93
jas497 <8@case.edu>
2016-12-26 22:14:25 -0500

    Can convert data to Caffe format.

--- final/final.py
+++ final/final.py
@@ -4,12 +4,24 @@ import caffe
 
 import zxc251_reader
 
-train_dataset, train_labels,\
-	valid_dataset, valid_labels,\
-	test_dataset, test_labels\
+#train_dataset, train_labels,\
+#	valid_dataset, valid_labels,\
+#	test_dataset, test_labels\
+d \
 	= zxc251_reader.get_data()
 
-print('Training:', train_dataset.shape, train_labels.shape)
-print('Validation:', valid_dataset.shape, valid_labels.shape)
-print('Testing:', test_dataset.shape, test_labels.shape)
+#print('Training:', train_dataset.shape, train_labels.shape)
+#print('Validation:', valid_dataset.shape, valid_labels.shape)
+#print('Testing:', test_dataset.shape, test_labels.shape)
+
+import caffe
+#import leveldb
+from caffe.proto import caffe_pb2
+
+for a, n in zip(d, ['trd','trl','vd','vl','ted','tel']):
+	print "doing " + n
+	blobproto = caffe.io.array_to_blobproto(a)
+	f = open(n + '.bin', 'wb')
+	f.write(blobproto.SerializeToString())
+	f.close()
 
